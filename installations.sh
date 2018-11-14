f69dab701440d0222356b1de53bdecf64b66ad3d
Dave Whipp <davewhipp@users.noreply.github.com>
2018-11-14 10:08:53 +0200

    Comment out installations for interactive Matplotlib

--- installations.sh
+++ installations.sh
@@ -4,10 +4,10 @@
 cd /home/jovyan/work
 
 # Install stuff needed for interactive Matplotlib widget
-conda install -c conda-forge ipympl
-conda install -c conda-forge nodejs
-jupyter labextension install @jupyter-widgets/jupyterlab-manager
-jupyter labextension install jupyter-matplotlib
+#conda install -c conda-forge ipympl
+#conda install -c conda-forge nodejs
+#jupyter labextension install @jupyter-widgets/jupyterlab-manager
+#jupyter labextension install jupyter-matplotlib
 
 # git reflog requires a name and email if user is not in passwd
 # even if you're only cloning
