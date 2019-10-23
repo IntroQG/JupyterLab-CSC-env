8521f90ea51502f7d512c4493bf5954f3c8db390
Dave Whipp <davewhipp@users.noreply.github.com>
2019-10-23 13:26:35 +0300

    Update build for 2019

--- installations.sh
+++ installations.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
-# Change working directory
-cd /home/jovyan/work
-
-# Install stuff needed for interactive Matplotlib widget
-#conda install -c conda-forge ipympl
-#conda install -c conda-forge nodejs
-#jupyter labextension install @jupyter-widgets/jupyterlab-manager
-#jupyter labextension install jupyter-matplotlib
-
 # git reflog requires a name and email if user is not in passwd
 # even if you're only cloning
 export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
 
-# Clone teaching notebooks
-git clone https://github.com/introqg/notebooks.git
+# Change to working dir
+cd /home/jovyan/work
 
-# Go to notebooks folder
-cd /home/jovyan/work/notebooks/notebooks
+# Check for existince of introqg directory, make if missing
+QG_DIR="/home/jovyan/work/introqg"
+if [ ! -d "$QG_DIR" ]; then
+    mkdir $QG_DIR
+fi
+cd $QG_DIR
+
+# Create exercises directory if it doesn't exist
+EXERCISE_DIR="$QG_DIR/exercises"
+if [ ! -d "$EXERCISE_DIR" ]; then
+    mkdir $EXERCISE_DIR
+fi
+
+# Grab notebooks repository
+NOTEBOOK_DIR="$QG_DIR/notebooks"
+if [ -d "$NOTEBOOK_DIR" ]; then
+    # Change directories and pull
+    cd "$NOTEBOOK_DIR"
+    git pull origin master
+else
+    # Clone the notebooks directory
+    git clone https://github.com/introqg/notebooks.git
+fi
